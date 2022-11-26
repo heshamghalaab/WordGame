7e84123f1af5ad361af6b0d4e526a57d70dc0783
heshamghalaab <hesham_ghalaab@hotmail.com>
2022-11-27 00:07:24 +0200

    handling UI

--- WordGame/App/Game/View/GameView.swift
+++ WordGame/App/Game/View/GameView.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Correct attempts: 0")
+                        .fontWeight(.bold)
+                    Text("Wrong attempts: 0")
+                        .fontWeight(.bold)
+                }
+            }.padding(.horizontal, 16)
+            
+            Spacer()
+            
+            VStack(alignment: .center) {
+                Text("fiambrera")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(nil)
+                Text("lunch box")
+                    .font(.title2)
+                    .lineLimit(nil)
+            }.padding()
+            
+            Spacer()
+            HStack(spacing: 16) {
+                Button {
+                    print("Correct")
+                } label: {
+                    Text("Correct")
+                        .frame(maxWidth: .infinity, maxHeight: 44)
+                        .foregroundColor(.white)
+                        .background(Color.green)
+                        .cornerRadius(8)
+                }
+                
+                Button {
+                    print("Wrong")
+                } label: {
+                    Text("Wrong")
+                        .frame(maxWidth: .infinity, maxHeight: 44)
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(8)
+                }
+            }.padding(.horizontal, 16)
+        }
     }
 }
 
