6ae465e78e9ef9e311ea0597d0fee31ee7e353a9
heshamghalaab <hesham_ghalaab@hotmail.com>
2022-11-26 23:26:55 +0200

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by Ghalaab on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
