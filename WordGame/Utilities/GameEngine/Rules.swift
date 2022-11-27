68962d1cf4adf879b63eddda45cf64955b2c374e
hesham ghalaab <hesham_ghalaab@hotmail.com>
2022-11-27 12:07:16 +0200

    [Milestone 2] Rules (#2)
    * Adding new rules to end the game.
    * adding a time limit for the question.
    * Update README.md

--- WordGame/Utilities/GameEngine/Rules.swift
+++ WordGame/Utilities/GameEngine/Rules.swift
@@ -9,9 +9,25 @@ import Foundation
 
 struct Rules {
     let probabilityOfCorrectness: Int
+    let maximumNumberOfQuestion: Int
+    let maximumNumberOfWrongAttempts: Int
+    let timeLimit: Int
     
-    /// Will accept only the range from 0...100 probabilityOfCorrectness Else will add the default Value.
-    init(probabilityOfCorrectness: Int = 25) {
+    /// - Parameters:
+    ///   - probabilityOfCorrectness: Will accept only the range from 0...100 probabilityOfCorrectness Else will add the **default value == 25**.
+    ///   - maximumNumberOfQuestion: Number of questions that should be presented to the user **default value == 15**.
+    ///   - maximumNumberOfWrongAttempts: Number of wrong attemps that should end the game **default value == 3**.
+    ///   - timeLimit: Time Limit for the question to end and to be marker as a wrong answer **default value == 5**.
+    init(
+        probabilityOfCorrectness: Int = 25,
+        maximumNumberOfQuestion: Int = 15,
+        maximumNumberOfWrongAttempts: Int = 3,
+        timeLimit: Int = 5
+    ) {
+        self.maximumNumberOfQuestion = maximumNumberOfQuestion
+        self.maximumNumberOfWrongAttempts = maximumNumberOfWrongAttempts
+        self.timeLimit = timeLimit
+        
         if probabilityOfCorrectness >= 0, probabilityOfCorrectness <= 100 {
             self.probabilityOfCorrectness = probabilityOfCorrectness
         } else {
